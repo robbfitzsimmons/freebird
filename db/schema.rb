44f5c3042c5674af791b7dce1ed0afa71b5c1814
Robb Fitzsimmons <robbfitzsimmons@gmail.com>
2015-05-16 17:52:01 -0400

    Migration: Itinerary, user fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516191157) do
+ActiveRecord::Schema.define(version: 20150516215112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "itineraries", force: :cascade do |t|
+    t.string   "airline"
+    t.integer  "flight_number"
+    t.date     "date"
+    t.string   "origin"
+    t.string   "destination"
+    t.boolean  "roundtrip"
+    t.decimal  "price"
+    t.integer  "user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "itineraries", ["user_id"], name: "index_itineraries_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -31,9 +46,12 @@ ActiveRecord::Schema.define(version: 20150516191157) do
     t.datetime "updated_at"
     t.string   "name"
     t.integer  "role"
+    t.string   "gender"
+    t.date     "birthday"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "itineraries", "users"
 end
